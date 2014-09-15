7e62acc3e08cb4144c5adb9f60145387919dacf2
Andrew Garner <andrewgarner@surefire.local>
2014-09-15 15:58:26 +0100

    Add basic authentication

--- app.rb
+++ app.rb
@@ -7,6 +7,10 @@ require 'sass/plugin/rack'
 
 Sass.load_paths << Gem.loaded_specs['govuk_frontend_toolkit'].full_gem_path + '/app/assets/stylesheets'
 
+use Rack::Auth::Basic, 'Prototype' do |username, password|
+  [username, password] == [ENV['AUTH_USERNAME'], ENV['AUTH_PASSWORD']]
+end if settings.production?
+
 use Sass::Plugin::Rack
 use Slimmer::App
 
