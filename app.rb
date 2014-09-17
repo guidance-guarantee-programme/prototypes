1d07b00f6a619ad2a4e40174c0f86532a09fb516
Alistair Duggin <alistair.duggin@btinternet.com>
2014-09-17 15:05:50 +0100

    Integrate gov_uk elements using bower

--- app.rb
+++ app.rb
@@ -6,9 +6,12 @@ require 'sinatra/content_for'
 require 'sinatra/reloader' if development?
 
 require 'slimmer'
+require 'sass/plugin'
 require 'sass/plugin/rack'
 
 Sass.load_paths << Gem.loaded_specs['govuk_frontend_toolkit'].full_gem_path + '/app/assets/stylesheets'
+Sass::Plugin.add_template_location('bower_components/govuk_elements/sass')
+
 
 use Rack::Auth::Basic, 'Prototype' do |username, password|
   [username, password] == [ENV['AUTH_USERNAME'], ENV['AUTH_PASSWORD']]
