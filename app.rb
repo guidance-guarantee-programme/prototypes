39b367b9d44feb5a6fc5adcb433a188baeb6241a
Jiannis Georgiadis <jiannis.georgiadis@moneyadviceservice.org.uk>
2014-10-01 18:00:32 +0100

    Change robot voice to Alice with British accent.

--- app.rb
+++ app.rb
@@ -131,7 +131,7 @@ post '/reminder-call', provides: ['xml'] do
   builder do |xml|
     xml.instruct!
     xml.Response do
-      xml.Say "Hi #{name}, your pension guidance session is on ... Keep calm. Read the guidance. Buy a smart electric bike. Enjoy your weekend!"
+      xml.Say "Hi #{name}, your pension guidance session is on ... Keep calm. Read the guidance. Buy a smart electric bike. Enjoy your weekend!", voice: 'alice', language: 'en-GB'
     end
   end
 end
