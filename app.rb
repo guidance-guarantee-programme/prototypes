b48c181f8e02f04c1bbad29fc5822542b85f8984
Alistair Duggin <alistair.duggin@btinternet.com>
2014-10-01 16:18:18 +0100

    Fix multipage title and nav padding

--- app.rb
+++ app.rb
@@ -121,6 +121,7 @@ end
 get '/your-options/:option' do
   @hide_session_promo = false
   @page = params[:option]
+  @page_title = "Your pension options"
   @your_option_page_title_1 = "Overview"
   @your_option_page_title_2 = "Taking cash"
   @your_option_page_title_3 = "Taking money out when you need it"
