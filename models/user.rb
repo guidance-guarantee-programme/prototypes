32f8f604b57ea463304831337b329253c7272601
Jiannis Georgiadis <jiannis.georgiadis@moneyadviceservice.org.uk>
2014-11-02 10:15:19 +0000

    Add memorable word as a security check.

--- models/user.rb
+++ models/user.rb
@@ -3,7 +3,7 @@ require 'active_model'
 class User
   include ActiveModel::Model
 
-  validates_presence_of :name, :surname, :email, :phone
+  validates_presence_of :name, :surname, :email, :phone, :secret
 
-  attr_accessor :name, :surname, :email, :phone
+  attr_accessor :name, :surname, :email, :phone, :secret
 end
