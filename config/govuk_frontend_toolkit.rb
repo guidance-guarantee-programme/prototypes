7943919f6d71480f182957bd12da945a5c33e942
Jiannis Georgiadis <jiannis.georgiadis@moneyadviceservice.org.uk>
2014-11-02 17:07:37 +0000

    Explicitly require govuk frontend toolkit.

--- config/govuk_frontend_toolkit.rb
+++ config/govuk_frontend_toolkit.rb
@@ -1,5 +1,6 @@
 require 'rails/engine'
 require 'sass/plugin/rack'
+require 'govuk_frontend_toolkit'
 
 Sass.load_paths << Gem.loaded_specs['govuk_frontend_toolkit'].full_gem_path + '/app/assets/stylesheets'
 Sass::Plugin.add_template_location('bower_components/govuk_elements/public/sass')
