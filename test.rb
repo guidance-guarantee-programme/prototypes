1dfebeb8e5f2ba1183f5b1cc96972a52b1ee3986
Jiannis Georgiadis <jiannis.georgiadis@moneyadviceservice.org.uk>
2014-11-04 16:44:36 +0000

    Add small test to exercise the app:
    - Book a phone appointment
    - Find a local branch
    - Use the tax calculator
    
    @andrewgarner what’s the best way to run this? Before deploying? On CI?

--- test.rb
+++ test.rb
@@ -1,11 +1,12 @@
+require 'dotenv'
+Dotenv.load
+
 ENV['RACK_ENV'] = 'test'
 
 require_relative 'config/application'
 require_relative 'app'
 
 require 'minitest/autorun'
-
-Dotenv.load
 
 class SmokeTest <  Minitest::Test
   include Capybara::DSL
@@ -22,5 +23,59 @@ class SmokeTest <  Minitest::Test
     visit '/'
 
     assert page.has_content? 'The Pensions Guidance Service'
+  end
+
+  def test_book_appointment
+    visit '/'
+    click_on('speak to a pension expert')
+    click_on('Start now')
+    click_on('Book a phone session')
+
+    # Pick a date and time
+    select('Wednesday, 17 December - 4:00pm', from: 'slots[]')
+    click_on('Continue')
+
+    # Fill in contact details
+    within('.form') do
+      fill_in('First name', with: 'Clark')
+      fill_in('Last name', with: 'Kent')
+      fill_in('Email', with: 'clark.kent@gmail.com')
+      fill_in('Phone number to call you on', with: '07460123456')
+      fill_in('Memorable word', with: 'cryptonite')
+    end
+
+    click_on('Continue')
+
+    # Confirm booking
+    click_on('Confirm booking')
+
+    assert page.has_content? 'You’ve successfully booked a pension guidance session'
+  end
+
+  def test_find_local_branch
+    visit '/'
+    click_on('speak to a pension expert')
+    click_on('Start now')
+
+    # Fill in postcode
+    within('.form') do
+      fill_in('Postcode', with: 'SW18 4XP')
+    end
+
+    click_on('Find a local branch')
+
+    assert page.has_content? 'Tooting'
+  end
+
+  def test_tax_calculator
+    visit '/pension-tax-calculator'
+
+    # Fill in pension and income
+    fill_in('pension', with: '10000')
+    fill_in('income', with: '10000')
+
+    click_on('Calculate')
+
+    assert page.has_content? 'Total amount you may get after tax'
   end
 end
