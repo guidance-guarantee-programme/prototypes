f057dff705c1ed83076638739894944ac6003a32
Jiannis Georgiadis <jiannis.georgiadis@moneyadviceservice.org.uk>
2014-11-04 13:38:36 +0000

    Update content for “your options” article.
    See: https://docs.google.com/a/digital.cabinet-office.gov.uk/document/d/1I2ZtD4wmxezh6euLPW9aYZ6kdo3yhpz76SwVPcSzs0E/edit

--- app.rb
+++ app.rb
@@ -148,11 +148,11 @@ class Prototype < Sinatra::Base
     @hide_session_promo = false
     @page = params[:option]
     @page_title = "Ways to take money from your pension pot"
-    @your_option_page_title_1 = "Overview"
+    @your_option_page_title_1 = "Options you have"
     @your_option_page_title_2 = "Taking a tax-free lump sum"
     @your_option_page_title_3 = "Taking the whole pot as cash"
-    @your_option_page_title_4 = "Taking money out when you need it"
-    @your_option_page_title_5 = "Buying a regular income"
+    @your_option_page_title_4 = "Taking money out when you need it (income drawdown)"
+    @your_option_page_title_5 = "Getting a guaranteed income (annuity)"
     @your_option_page_title_6 = "Mixing your pension options"
     erb :"articles/your_options/#{params[:option]}"
   end
